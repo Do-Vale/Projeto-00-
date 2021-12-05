b96e10579dbd6ebe91c1a73659ae2db259c94b29
Lek <alexsandrodovale1994@gmail.com>
2021-12-05 18:07:59 -0300

    first function JS

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("email2").value = document.getElementById("email1").value;
+  document.getElementById("password2").value = document.getElementById("password1").value;
+  document.getElementById("email1").value = "";
+  document.getElementById("password1").value = ""; 
+ }
