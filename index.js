d660435be79e0a490b276b53269f7f3923b78bba
Lek <alexsandrodovale1994@gmail.com>
2021-12-06 23:55:17 -0300

    second version JS

--- index.js
+++ index.js
@@ -3,4 +3,29 @@ function myFunction() {
   document.getElementById("password2").value = document.getElementById("password1").value;
   document.getElementById("email1").value = "";
   document.getElementById("password1").value = ""; 
+  document.getElementById("emailHelp").style.visibility = "hidden";
+  document.getElementById("myBtn").disabled = true;
+  document.getElementById("demo").innerHTML = " Vazooou!";  
  }
+
+
+/* Remover o elemento selecionado do documento
+var myobj = document.getElementById("demo");
+myobj.remove();
+
+
+function changeVisibility() {
+  document.getElementById("emailHelp").style.visibility = "hidden";
+
+
+           FUNÇÃO PARA RESETAR 
+
+  COLOCAR NA DIV : <button onclick="resetElement()">Reset All</button>
+
+  function resetElement() {
+  document.getElementById("imgbox1").style.display = "block";
+  document.getElementById("imgbox2").style.visibility = "visible";
+}  
+  https://www.w3schools.com/css/tryit.asp?filename=trycss_display
+
+  */
